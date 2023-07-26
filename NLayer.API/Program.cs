7362a90476474904e374b834f0038171eca49b34
Omer UNAL <omer.unal4@saglik.gov.tr>
2023-07-26 15:28:52 +0300

    Mapper'lama

--- NLayer.API/Program.cs
+++ NLayer.API/Program.cs
@@ -5,6 +5,8 @@ using NLayer.Core.UnitOfWorks;
 using NLayerRepository;
 using NLayerRepository.Reporsitories;
 using NLayerRepository.UnitOfWorks;
+using NLayerService.Mapping;
+using NLayerService.Services;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,12 +14,18 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+#region benim eklediklerim
+
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-//builder.Services.AddScoped(typeof(IService<>), typeof(Service<>));
+builder.Services.AddScoped(typeof(IService<>), typeof(Service<>));
+builder.Services.AddAutoMapper(typeof(MapProfile));
+
+
+
 builder.Services.AddDbContext<AppDbContext>(x =>
 {
     x.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection"), options =>
@@ -26,6 +34,8 @@ builder.Services.AddDbContext<AppDbContext>(x =>
 
     });
 });
+
+#endregion
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
---
+++ NLayerService/Mapping/MapProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using NLayer.Core.DTOs;
+using NLayer.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerService.Mapping
+{
+    public class MapProfile : Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<Product, ProductDTO>().ReverseMap(); //reversemap dersem productDtoyu product da çevirebilirim
+            CreateMap<Category, CategoryDTO>().ReverseMap();
+            CreateMap<ProductFeature, ProductFeatureDTO>().ReverseMap();
+
+            CreateMap<ProductUpdateDTO, Product>();
+        }
+    }
+}
