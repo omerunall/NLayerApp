cdaea6a74246433bcbc94d59d803d67246a15972
Omer UNAL <omer.unal4@saglik.gov.tr>
2023-10-10 11:36:31 +0300

    ...

---
+++ NLayer.API/Controllers/CategoriesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using NLayer.Core.Services;
+
+namespace NLayer.API.Controllers
+{
+
+    public class CategoriesController : CustomBaseController
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+
+        //api/categories/GetSingleCategoryByIdWithsProduct/id
+        [HttpGet("[action]")]
+        public async Task<IActionResult> GetSingleCategoryByIdWithsProduct(int categoryId)
+        {
+            return CreateActionResult(await _categoryService.GetSingleCategoryByIdWithProductsAsync(categoryId));
+        }
+    }
+}
---
+++ NLayerService/Validation/ProductDtoValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using NLayer.Core.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerService.Validation
+{
+    public class ProductDtoValidator : AbstractValidator<ProductDTO>
+    {
+        public ProductDtoValidator()
+        {
+            RuleFor(x => x.Name).NotNull().WithMessage("{PropertyName} gerekli").NotEmpty().WithMessage("{PropertyName} gereklidir.");
+
+            RuleFor(x => x.Price).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} 0 dan büyük olmalı");
+
+            RuleFor(x => x.Stock).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} 0 dan büyük olmalı");
+
+            RuleFor(x => x.CategoryId).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} 0 dan büyük olmalı");
+        }
+    }
+}
